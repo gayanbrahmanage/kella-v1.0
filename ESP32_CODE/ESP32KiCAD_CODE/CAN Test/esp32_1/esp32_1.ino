a9b18b0f0cd6cd4bfb66643da498d9516b3ae218
Gayan Brahmanage <gayansampathefac@gmail.com>
2024-09-04 12:53:31 -0600

    Esp32code updated

--- ESP32_CODE/ESP32KiCAD_CODE/CAN Test/esp32_1/esp32_1.ino
+++ ESP32_CODE/ESP32KiCAD_CODE/CAN Test/esp32_1/esp32_1.ino
@@ -1,69 +1,37 @@
-#include <mcp2515.h>
 #include <SPI.h>
+#include <mcp2515.h>
 
-struct can_frame canMsg;
+struct can_frame canMsg1;
 
 MCP2515 mcp2515(5);
 
-// set pin numbers
-
-const int buttonPin = 27; 
-const int ledPin1 =  12;   
-const int ledPin2 =  14;   
-// Potentiometer is connected to GPIO 34 (Analog ADC1_CH6) 
-const int potPin = 32;
-
-int potValue = 0;
-int buttonState = 0;
 
 void setup() {
-
-  SPI.begin(18, 19, 23, 5);  // Initialize SPI with SCK, MISO, MOSI, CS
-  mcp2515.reset();
-  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);  // Set CAN bus speed
-  mcp2515.setNormalMode();
-  
-  canMsg.can_id  = 0x00;
-  canMsg.can_dlc = 8;
-  canMsg.data[0] = 0x8E;
-  canMsg.data[1] = 0x87;
-  canMsg.data[2] = 0x32;
-  canMsg.data[3] = 0xFA;
-  canMsg.data[4] = 0x26;
-  canMsg.data[5] = 0x8E;
-  canMsg.data[6] = 0xBE;
-  canMsg.data[7] = 0x86;
-
-
-  // initialize 
-  pinMode(buttonPin, INPUT);
-  // initialize the LED pin as an output
-  pinMode(ledPin1, OUTPUT);
-  // initialize the LED pin as an output
-  pinMode(ledPin2, OUTPUT);
+  canMsg1.can_id  = 0x0F6;
+  canMsg1.can_dlc = 8;
+  canMsg1.data[0] = 0x8E;
+  canMsg1.data[1] = 0x87;
+  canMsg1.data[2] = 0x32;
+  canMsg1.data[3] = 0xFA;
+  canMsg1.data[4] = 0x26;
+  canMsg1.data[5] = 0x8E;
+  canMsg1.data[6] = 0xBE;
+  canMsg1.data[7] = 0x86;
 
   Serial.begin(115200);
-  delay(1000);
+  
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_125KBPS);
+  mcp2515.setNormalMode();
+  
+  Serial.println("Example: Write to CAN");
 }
 
 void loop() {
-
-  mcp2515.sendMessage(&canMsg);
-  Serial.println("Message sent CAN");
   
-  // Reading potentiometer value
-  potValue = analogRead(potPin);
-  Serial.println(potValue);
-  delay(500);
+  mcp2515.sendMessage(&canMsg1);
+
+  Serial.println("Messages sent");
   
-  buttonState = digitalRead(buttonPin);
-
-  if (buttonState == HIGH) {
-    digitalWrite(ledPin1, HIGH);
-    digitalWrite(ledPin2, LOW);
-  }else{
-    digitalWrite(ledPin1, LOW);
-    digitalWrite(ledPin2, HIGH);
-  }
-
+  delay(1000);
 }
